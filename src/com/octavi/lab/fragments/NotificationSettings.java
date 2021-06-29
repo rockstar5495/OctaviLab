ced97ed0b9cefea7e8cd5ffacc09ea9a75b960c1
Pranav Vashi <neobuddy89@gmail.com>
2021-06-29 10:25:32 +0530

    OctaviLab: Alert Slider: Add toggle to disable notifications [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- src/com/octavi/lab/fragments/NotificationSettings.java
+++ src/com/octavi/lab/fragments/NotificationSettings.java
@@ -2,18 +2,35 @@ package com.octavi.lab.fragments;
 
 import com.android.internal.logging.nano.MetricsProto;
 
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.os.Bundle;
 import com.android.settings.R;
 
+import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
+import androidx.preference.SwitchPreference;
 
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.SearchIndexable;
+
+import java.util.List;
 
 public class NotificationSettings extends SettingsPreferenceFragment {
 
+    private static final String ALERT_SLIDER_PREF = "alert_slider_notifications";
+
     private Preference mChargingLeds;
+    private Preference mAlertSlider;
+
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -21,7 +38,16 @@ public class NotificationSettings extends SettingsPreferenceFragment {
 
         addPreferencesFromResource(R.xml.octavi_lab_notifications);
 
-        PreferenceScreen prefScreen = getPreferenceScreen();
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+        final Context mContext = getActivity().getApplicationContext();
+        final ContentResolver resolver = mContext.getContentResolver();
+        final Resources res = mContext.getResources();
+
+        mAlertSlider = (Preference) prefScreen.findPreference(ALERT_SLIDER_PREF);
+        boolean mAlertSliderAvailable = res.getBoolean(
+                com.android.internal.R.bool.config_hasAlertSlider);
+        if (!mAlertSliderAvailable)
+            prefScreen.removePreference(mAlertSlider);
 
         mChargingLeds = (Preference) findPreference("charging_light");
         if (mChargingLeds != null
@@ -31,8 +57,35 @@ public class NotificationSettings extends SettingsPreferenceFragment {
         }
     }
 
+    public static void reset(Context mContext) {
+        ContentResolver resolver = mContext.getContentResolver();
+        Settings.System.putIntForUser(resolver,
+                Settings.System.ALERT_SLIDER_NOTIFICATIONS, 1, UserHandle.USER_CURRENT);
+    }
+
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.OCTAVI;
     }
+
+    /**
+     * For search
+     */
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.octavi_lab_notifications) {
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    List<String> keys = super.getNonIndexableKeys(context);
+
+                    final Resources res = context.getResources();
+
+                    boolean mAlertSliderAvailable = res.getBoolean(
+                            com.android.internal.R.bool.config_hasAlertSlider);
+                    if (!mAlertSliderAvailable)
+                        keys.add(ALERT_SLIDER_PREF);
+
+                    return keys;
+                }
+            };
 }
