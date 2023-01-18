5e49b8f0335a448faead96a8e8fd1c84394b22f4
ezio84 <brabus84@gmail.com>
2023-01-18 14:38:53 +0000

    OctaviLab: Battery light customization [2/2]

--- src/com/octavi/lab/fragments/NotificationSettings.java
+++ src/com/octavi/lab/fragments/NotificationSettings.java
@@ -13,11 +13,22 @@ import com.android.settings.SettingsPreferenceFragment;
 
 public class NotificationSettings extends SettingsPreferenceFragment {
 
+    private Preference mChargingLeds;
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.octavi_lab_notifications);
+
+        PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mChargingLeds = (Preference) findPreference("charging_light");
+        if (mChargingLeds != null
+                && !getResources().getBoolean(
+                        com.android.internal.R.bool.config_intrusiveBatteryLed)) {
+            prefScreen.removePreference(mChargingLeds);
+        }
     }
 
     @Override
