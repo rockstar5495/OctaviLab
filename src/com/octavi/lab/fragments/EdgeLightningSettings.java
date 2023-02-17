d4b2225547da5abc800dd104e1a89162f10abbe1
idoybh <idoybh@users.noreply.github.com>
2023-02-17 11:48:07 +0000

    OctaviLab: Add ambient edge lightning settings

---
+++ src/com/octavi/lab/fragments/EdgeLightningSettings.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright (C) 2020 Yet Another AOSP Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.octavi.lab.fragments;
+
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.util.TypedValue;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+import com.octavi.support.preference.CustomSeekBarPreference;
+import com.octavi.support.preference.SecureSettingSwitchPreference;
+import com.octavi.support.preference.SystemSettingListPreference;
+import com.octavi.support.preference.SystemSettingSwitchPreference;
+
+public class EdgeLightningSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String AMBIENT_LIGHT_COLOR = "ambient_notification_color_mode";
+    private static final String AMBIENT_LIGHT_CUSTOM_COLOR = "ambient_notification_light_color";
+    private static final String AMBIENT_LIGHT_DURATION = "ambient_notification_light_duration";
+    private static final String AMBIENT_LIGHT_REPEAT_COUNT = "ambient_notification_light_repeats";
+
+    private SystemSettingListPreference mEdgeLightColorMode;
+    private ColorPickerPreference mEdgeLightColor;
+    private CustomSeekBarPreference mEdgeLightDuration;
+    private CustomSeekBarPreference mEdgeLightRepeatCount;
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        addPreferencesFromResource(R.xml.edge_lightning_settings);
+        mEdgeLightColorMode = (SystemSettingListPreference) findPreference(AMBIENT_LIGHT_COLOR);
+        int edgeLightColorMode = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.NOTIFICATION_PULSE_COLOR_MODE, 0, UserHandle.USER_CURRENT);
+        mEdgeLightColorMode.setValue(String.valueOf(edgeLightColorMode));
+        mEdgeLightColorMode.setSummary(mEdgeLightColorMode.getEntry());
+        mEdgeLightColorMode.setOnPreferenceChangeListener(this);
+
+        mEdgeLightColor = (ColorPickerPreference) findPreference(AMBIENT_LIGHT_CUSTOM_COLOR);
+        int edgeLightColor = Settings.System.getInt(getContentResolver(),
+                Settings.System.NOTIFICATION_PULSE_COLOR, 0xFFFFFFFF);
+        mEdgeLightColor.setNewPreviewColor(edgeLightColor);
+        String edgeLightColorHex = String.format("#%08x", (0xFFFFFFFF & edgeLightColor));
+        if (edgeLightColorHex.equals("#ffffffff")) {
+            mEdgeLightColor.setSummary(R.string.default_string);
+        } else {
+            mEdgeLightColor.setSummary(edgeLightColorHex);
+        }
+        mEdgeLightColor.setOnPreferenceChangeListener(this);
+
+        mEdgeLightDuration = (CustomSeekBarPreference) findPreference(AMBIENT_LIGHT_DURATION);
+        int lightDuration = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.NOTIFICATION_PULSE_DURATION, 2, UserHandle.USER_CURRENT);
+        mEdgeLightDuration.setValue(lightDuration);
+        mEdgeLightDuration.setOnPreferenceChangeListener(this);
+
+        mEdgeLightRepeatCount = (CustomSeekBarPreference) findPreference(AMBIENT_LIGHT_REPEAT_COUNT);
+        int edgeLightRepeatCount = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.NOTIFICATION_PULSE_REPEATS, 0, UserHandle.USER_CURRENT);
+        mEdgeLightRepeatCount.setValue(edgeLightRepeatCount);
+        mEdgeLightRepeatCount.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mEdgeLightColorMode) {
+            int edgeLightColorMode = Integer.valueOf((String) newValue);
+            int index = mEdgeLightColorMode.findIndexOfValue((String) newValue);
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.NOTIFICATION_PULSE_COLOR_MODE, edgeLightColorMode, UserHandle.USER_CURRENT);
+            mEdgeLightColorMode.setSummary(mEdgeLightColorMode.getEntries()[index]);
+            if (edgeLightColorMode == 3) {
+                mEdgeLightColor.setEnabled(true);
+            } else {
+                mEdgeLightColor.setEnabled(false);
+            }
+            return true;
+        } else if (preference == mEdgeLightColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            if (hex.equals("#ffffffff")) {
+                preference.setSummary(R.string.default_string);
+            } else {
+                preference.setSummary(hex);
+            }
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.NOTIFICATION_PULSE_COLOR, intHex);
+            return true;
+        } else if (preference == mEdgeLightDuration) {
+            int value = (Integer) newValue;
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.NOTIFICATION_PULSE_DURATION, value, UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference == mEdgeLightRepeatCount) {
+            int value = (Integer) newValue;
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.NOTIFICATION_PULSE_REPEATS, value, UserHandle.USER_CURRENT);
+            return true;
+        }
+    return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.OCTAVI;
+    }
+}
+
